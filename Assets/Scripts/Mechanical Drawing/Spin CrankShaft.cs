809b6482242141e8a61b6cb2283942623b147592
OliveJar <encru1010@gmail.com>
2025-01-20 01:31:22 -0500

    Added controller inputs

--- Assets/Scripts/Mechanical Drawing/Spin CrankShaft.cs
+++ Assets/Scripts/Mechanical Drawing/Spin CrankShaft.cs
@@ -8,8 +8,10 @@ public class SpinCrankShaft : MonoBehaviour
     private Transform Rotatepoint;
     public AnimationCurve curve;
 
-    [Range(0, 1000)]
+    //[Range(0, 1000)]
     public float rotatespeed1;
+    public bool usingMouse;
+    public bool usingController;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,27 @@ public class SpinCrankShaft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(usingMouse)
+        {
+            usingController = false;
+
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rotatespeed1 = Mathf.Clamp(((mouse.y)+2)*250, 0, 1000);
+
+            if(Input.GetButton("Fire1"))
+            {
+                rotatespeed1 = 2000;
+            }
+        }
+        if (usingController)
+        {
+            usingMouse = false;
+            rotatespeed1 = Mathf.Clamp((Input.GetAxis("Mouse Y") * 10000), 0, 1000);
+            if(Input.GetButton("Fire1") && (Input.GetAxis("Mouse Y") >= 0.1))
+            {
+                rotatespeed1 = 2000;
+            }
+        }
         transform.RotateAround(Rotatepoint.transform.position, Vector3.back, curve.Evaluate(rotatespeed1) * Time.deltaTime);
     }
 }
