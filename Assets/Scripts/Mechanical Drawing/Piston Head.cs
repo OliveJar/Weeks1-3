4d31edf0061782e380a0e1b62784d32c7f04e38d
OliveJar <encru1010@gmail.com>
2025-01-27 13:23:33 -0500

    Added multiple pistons in a classic I4 pattern (final scene update)

--- Assets/Scripts/Mechanical Drawing/Piston Head.cs
+++ Assets/Scripts/Mechanical Drawing/Piston Head.cs
@@ -8,11 +8,12 @@ public class PistonHead : MonoBehaviour
     [SerializeField]
     //Gets the location of the crankpin journals (the point of connection of the connecting rod and the crankshaft)
     private Transform Rotatepoint2;
+    public float pistonX;
 
     // Update is called once per frame
     void Update()
     {
         //Makes the piston move up and down according to the position of the crankpin journals
-        transform.position = new Vector2(-0.7055448f, (Rotatepoint2.transform.position.y) + 2.444462f);
+        transform.position = new Vector2(pistonX, (Rotatepoint2.transform.position.y) + 2.444462f);
     }
 }
